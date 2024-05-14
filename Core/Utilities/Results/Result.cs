e0e09989457364ca705c234d48141c15336ffdc1
Hasan Can Sevim <shasancan0@gmail.com>
2024-05-14 23:56:09 +0300

    14.5.2024

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        // this -> result olduğu için alttakı kodun anlamı : result'ın tek parametreli olan constructor'ına success'i yolla.Yani bu constructor çalıştığında diğer constructor da çalışmış olacak bu yüzden success işlemini orda yaptık.
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+            //Success = success;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
